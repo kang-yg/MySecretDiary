acd56e6d1dbfffb48adae69210dfd2b299ec995f
YoungGyu <ygkang@crscube.co.kr>
2021-06-30 17:36:34 +0900

    잠금화면 비밀번호 확인 및 수정

--- app/src/main/java/com/kyg/mysecretdiary/MainActivity.kt
+++ app/src/main/java/com/kyg/mysecretdiary/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.kyg.mysecretdiary
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.NumberPicker
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
@@ -10,8 +14,8 @@ import androidx.databinding.DataBindingUtil
 import com.kyg.mysecretdiary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    val MY_SHAREDPREFERENCES = "MY_SHAREDPREFERENCES"
-    val PASSWORD_KEY = "PASSWORD"
+    private val MY_SHAREDPREFERENCES = "MY_SHAREDPREFERENCES"
+    private val PASSWORD_KEY = "PASSWORD"
 
     lateinit var activityMainBinding: ActivityMainBinding
 
@@ -23,14 +27,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val numberPicker2: NumberPicker by lazy {
-        activityMainBinding.numberPicker1.apply {
+        activityMainBinding.numberPicker2.apply {
             minValue = 0
             maxValue = 9
         }
     }
 
     private val numberPicker3: NumberPicker by lazy {
-        activityMainBinding.numberPicker1.apply {
+        activityMainBinding.numberPicker3.apply {
             minValue = 0
             maxValue = 9
         }
@@ -44,25 +48,101 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.changePasswordButton
     }
 
+    private val sharedPreference: SharedPreferences by lazy {
+        getSharedPreferences(MY_SHAREDPREFERENCES, Context.MODE_PRIVATE)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        initViews()
+    }
+
+    private fun initViews() {
+        setNumberPicker()
         setOpenButton()
+        setChangePasswordButton()
+    }
+
+    private fun setNumberPicker() {
+        numberPicker1
+        numberPicker2
+        numberPicker3
     }
 
     private fun setOpenButton() {
         openButton.setOnClickListener {
-            val sharedPreference = getSharedPreferences(MY_SHAREDPREFERENCES, Context.MODE_PRIVATE)
+            if (changePasswordButton.isPressed) {
+                // 비밀번호 변경 중인경우
+                showToast(this, resources.getString(R.string.PASSWORD_CHANGING));
 
-            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
-            val passwordFromSharedPreference = sharedPreference.getString(PASSWORD_KEY, "000")
-
-            if (passwordFromSharedPreference.equals(passwordFromUser)) {
-
+                return@setOnClickListener
             } else {
-                AlertDialog.Builder(this).setTitle(resources.getString(R.string.fail))
+                if (checkPassword()) {
+                    // 비밀번호가 맞은 경우
+
+                } else {
+                    // 비밀번호가 틀린 경우
+                    val titleFromResource = resources.getString(R.string.PASSWORD_FAIL_TITLE)
+                    val messageFromResource = resources.getString(R.string.PASSWORD_FAIL_CONTENT)
+                    showAlertDialog(titleFromResource, messageFromResource)
+                }
             }
         }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setChangePasswordButton() {
+        changePasswordButton.setOnTouchListener { v, event ->
+            when (event?.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    if (!changePasswordButton.isPressed) {
+                        // 비밀번호 변경이 활성화된 상태
+                        if (checkPassword()) {
+                            changePasswordButton.isPressed = !changePasswordButton.isPressed
+                        } else {
+                            val titleFromResource = resources.getString(R.string.PASSWORD_FAIL_TITLE)
+                            val messageFromResource = resources.getString(R.string.PASSWORD_FAIL_CONTENT)
+                            showAlertDialog(titleFromResource, messageFromResource)
+                        }
+                    } else {
+                        // 비밀번호 변경이 비활성화된 상태
+                        sharedPreference.edit().putString(
+                            PASSWORD_KEY,
+                            "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+                        ).apply()
+                        changePasswordButton.isPressed = !changePasswordButton.isPressed
+                    }
+
+                    return@setOnTouchListener true
+                }
+
+                else -> {
+                    return@setOnTouchListener true
+                }
+            }
+        }
+    }
+
+    private fun checkPassword(): Boolean {
+        val passwordFromUser =
+            "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+        val passwordFromSharedPreference =
+            sharedPreference.getString(PASSWORD_KEY, "000")
+
+        return (passwordFromSharedPreference.equals(passwordFromUser))
+    }
+
+    private fun showToast(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showAlertDialog(title: String, message: String) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(resources.getString(R.string.PASSWORD_POSITIVE)) { _, _ -> }
+            .create().show()
     }
 }
